480861fd920b848dbad1400e0c9502574d7ec396
Jhon Grajales <grajalesjhonesteban@gmail.com>
2026-02-24 17:08:41 -0500

    v7

--- src/services/usuario.service.js
+++ src/services/usuario.service.js
@@ -6,7 +6,33 @@ const createUsuario = async (data) => {
 const ListarUsuarios = async () => {
     return await Usuario.findAll();
 };
+
+//obtener por el id
+const obtenerUsuarioPorId = async (id) => {
+    return await Usuario.findByPk(id);
+};
+//actualizar usuario
+const actualizarUsuario = async (id, data) => {
+    const usuario = await Usuario.findByPk(id);
+    if (!usuario) {
+        return null;
+    }
+    await usuario.update(data);
+    return usuario;
+};
+    //eliminar usuario
+const eliminarUsuario = async (id) => {
+    const usuario = await Usuario.findByPk(id);
+    if (!usuario) {
+        return null;
+    }
+    await usuario.destroy();
+    return true;
+};
 module.exports = {
     createUsuario,
-    ListarUsuarios
+    ListarUsuarios,
+    obtenerUsuarioPorId,
+    actualizarUsuario,
+    eliminarUsuario
 };
